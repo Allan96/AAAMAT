f7bb3836fbe08b2354f60da7d51f2d132cc4848d
Allan Souza <allan.souza.silva@usp.br>
2019-05-16 01:03:31 -0300

    80% concluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 console.log("Site desenvolvido por Allan Souza https://fb.com/ASDSAllan");
 $(".btn-scrool").click(function(o){o.preventDefault();var t=$(this).attr("href"),l=($("nav").innerHeight(),$(t).offset().top);$("html, body").animate({scrollTop:l},1500)});
+var $containerWidth = $(window).width();
+if ($containerWidth <= 992) {
+    $('.btn-scrool').click(function (e) { 
+        e.preventDefault();
+        if($('.navbar-collapse').hasClass('show')){
+            $('.navbar-collapse').removeClass('show');
+            $('.navbar-toggler').addClass('collapsed');
+        }
+        
+    });
+}
